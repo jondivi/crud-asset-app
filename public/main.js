f36bf3d7d66c3c9a78db1239383a2e8905ce4d4b
Jon Divi <jonathan.divi@gmail.com>
2022-06-19 17:18:29 -0400

    adding in delete functionality

--- public/main.js
+++ public/main.js
@@ -1,13 +1,62 @@
-//This is going to listen for when we click the 'update-button' in the index.ejs
+// const { response } = require("express")
 
-update.addEventListener('click',_=>{
+//This is going to listen for when we click the 'update-button' in the index.ejs
+const update = document.querySelector('#update-button')
+
+//This is going to listen for when we click the 'delete-button' in the index.ejs
+const deleteButton=document.querySelector('#delete-button')
+
+//This is going to be where we designate 'messages' for when there is nothing left to delete
+const messageDiv=document.querySelector('#message')
+
+
+//And this is going to perform an update action for us
+update.addEventListener('click', _ => {
     //The PUT Request will go here
-    fetch('assets',{
+    fetch('/items',{
         method: 'put',
-        headers:{'Content-Type':'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             name: 'Apple',
             model: 'MacBook Air'
         })
     })
+        .then(response=>{
+            if (response.ok) return response.json()
+        })
+        .then(response=>{
+            window.location.reload(true)
+            // console.log(response)
+        })
 })
+
+
+//This is going to perform a delete action for us
+deleteButton.addEventListener('click',_=>{
+    fetch('/items',{
+        method: 'delete',
+        headers:{ 'Content-Type': 'aplication/json'},
+        body:JSON.stringify({
+            name: 'take'
+        })
+    })
+    .then(response=>{
+        if(response.ok) return response.json()
+    })
+    .then(response=>{
+        if(response==='No items to delete'){
+            messageDiv.textContent= 'No more items to delete'
+        } else{
+            window.location.reload(true)
+        }
+    })
+    .catch(console.error)
+    // .then(data=>{
+    //     window.location.reload()
+    // })
+})
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-//server check
-console.log('Node is up and noding')
-
 //requirements for the server
 const express=require('express');
 const bodyParser=require('body-parser')
 const app=express();
 const MongoClient=require('mongodb').MongoClient
 const cors=require('cors')
-const { response } = require('express');
-const res = require('express/lib/response');
 const PORT=8000
+
+// const { response } = require('express');
+// const res = require('express/lib/response');
+// const req = require('express/lib/request');
+
+
+// Replace process.env.DB_URL with your actual connection string
+// const connectionString = process.env.DB_URL
 
     //This will let us setup a connection to our MongoDB database
     MongoClient.connect('mongodb+srv://jdivi:5utu1fa199ym@cluster0.syuqd.mongodb.net/?retryWrites=true&w=majority',{
@@ -19,34 +22,35 @@ const PORT=8000
             const db=client.db('assets')
             const assetCollection=db.collection('assets')
 
-                    ////////// Middlewares and other routes here:
+                    ////===================================
+                    // Middlewares and other routes here:
 
+                    
                     //This tells express that we are going to use EJS as the template engine
                     app.set('view engine', 'ejs')
-
-                    //This will tell express that we want to use cors
-                    app.use(cors())
-
-                    //This will tell express to make the 'public' folder accessible by using the built in middle-ware express.static
-                    app.use(express.static('public'))
+                    
+                    //This will tell the server that we are going to use body-parser. NEEDS TO BEFORE GETS AND POSTS!!!
+                    app.use(bodyParser.urlencoded({ extended: true }))
 
                     //This will teach the server to read JSON 
                     app.use(bodyParser.json())
 
-                    
-                    //This will tell the server that we are going to use body-parser. NEEDS TO BEFORE GETS AND POSTS!!!
-                    app.use(bodyParser.urlencoded({extended:true}))
+                    //This will tell express to make the 'public' folder accessible by using the built in middle-ware express.static
+                    app.use(express.static('public'))
 
-                    //////////
+                    //This will tell express that we want to use cors
+                    app.use(cors())
+
+                    
+                   ////===================================
 
                       
 
-
-             ///// CRUD BELOW:
+            ////===================================
+            // CRUD BELOW:
 
              //sets where GET requests go to
              app.get('/',(request,response)=>{
-                // const cursor=db.collection('assets').find()
                 db.collection('assets').find().toArray()
                     .then(results =>{
                         response.render('index.ejs',{assets: results})
@@ -59,7 +63,7 @@ const PORT=8000
              app.post('/items',(request,response)=>{
                  assetCollection.insertOne(request.body)
                      .then(result=>{
-                         console.log(result)
+                        //  console.log(result)
                          response.redirect('/')
                      })
                      .catch(error=>console.error(error))
@@ -67,13 +71,48 @@ const PORT=8000
 
             
              //sets how PUT requests work
-             app.put('/assets',(request,response)=>{
-                 console.log(request,response)
-             })
+             
+            app.put('/items',(request,response)=>{
+                // console.log(request.body)
+                assetCollection.findOneAndUpdate(
+                    { name: 'Yoda' },
+                    {
+                      $set: {
+                        name: request.body.name,
+                        model: request.body.model
+                      }
+                    },
+                    {
+                      upsert: true
+                    }
+                  )
+                .then(result => {
+                    response.json('Success')
+                })
+                .catch(error => console.error(error))
+                })
 
 
+            //sets how DELETE requests work
+            app.delete('/items',(request,response)=>{
+                assetCollection.deleteOne(
+                    { name: 'take' }
+                )
+                .then(result=>{
+                    if (result.deletedCount === 0) {
+                        return response.json('No items to delete')
+                      }
+                      response.json(`Deleted asset`)
+                    })
+                    .catch(error => console.error(error))
 
-             ///// CRUD ABOVE ^
+                    // response.json('Deleted Apple record')
+                // })
+                // .catch(error=>console.error(error))
+            })
+
+
+            ////===================================
 
  
              
